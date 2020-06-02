2bc44d7a9a5072a29c9d9c75eaba951aac87c583
CVASILITA <chiril.vasilita@orange.md>
2020-06-02 23:28:08 +0300

    changed return values

--- src/SimpleApi/Controllers/ValuesController.cs
+++ src/SimpleApi/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace SimpleApi.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "dotnet", "core", "and smth", "else" };
         }
 
         // GET api/values/5
